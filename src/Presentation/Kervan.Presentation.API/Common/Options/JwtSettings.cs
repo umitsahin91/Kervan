4e0b68125c1e58a848799bb04e2985d9954d0865
Ümit Şahin <umitsahin91@hotmail.com>
2025-07-17 00:08:14 +0300

    .

--- src/Presentation/Kervan.Presentation.API/Common/Options/JwtSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Kervan.Presentation.API.Common.Options;
-
-public class JwtSettings
-{
-    public string Issuer { get; init; }
-    public string Audience { get; init; }
-    public string Secret { get; init; }
-}
